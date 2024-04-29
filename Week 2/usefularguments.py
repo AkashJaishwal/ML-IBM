74051f435de1f914acbb5dd97c0d5112cc984cc1
Akash Jaiswal <akashjaiswal.com>
2024-04-29 23:38:08 +0530

    updated

---
+++ Week 2/usefularguments.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+
+filepath = 'D:\\Acadamic\\ML IBM\\Week 2\\CS 21-25.csv'
+
+#import the data
+data = pd.read_csv(filepath) # 'data' is a variable which store the value in it form the filepath
+
+#print a few rows 
+print (data.iloc[:5]) #we have accessed the file data and printed the rows
+
+# # Different delimiters - tab-separated file (.tsv):
+# data = pd.read_csv(filepath, sep='\t')
+
+# # Different delimiters - space-sepated file:
+# data = pd.read_csv(filepath, delim_whitespace=True)
+
+# #Don't use first row for coumn names
+# data = pd.read_csv(filepath, header=None)
+
+# #Specify column names:
+# data = pd.read_csv(filepath, names=['Name1', 'Name2'])
+
+# #custome missing vales:
+# data = pd.read_csv(filepath, na_values=['NA'])
